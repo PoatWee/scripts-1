78a7d8ef28cea25e705a87309e247ac231fd2516
微信公众号少年歌行PRO <69911266+sngxpro@users.noreply.github.com>
2020-09-07 16:11:41 +0800

    Update 2496.js

--- 2496.js
+++ 2496.js
@@ -1,5 +1,5 @@
 let obj = JSON.parse($response.body);
 
-obj.value.rules = {"expiration_time":2029-05-01,"vipstatus":1};
+obj.value.vip = {"expiration_time":2029-05-01,"vipstatus":1};
   
 $done({body: JSON.stringify(obj)});
